cd884dd92eeb0e8c7cb7f67b3e9d6feedc9fae8a
Emma-Hyejin <cjd191058@naver.com>
2023-03-31 13:49:44 +0900

    popup창 구현

--- todolist/src/Main.jsx
+++ todolist/src/Main.jsx
@@ -5,8 +5,21 @@ import './Component/Contents.css';
 import Footer from './Footer';
 import Sidebar from './Side/Sidebar';
 import Contents from './Component/Contents';
+import {useState} from 'react';
 
 const Main = () => {
+    const parseDate = new Date().toLocaleDateString('ko-kr');
+    const [showPopup, setShowpopup] = useState(false);
+
+    const togglePopup = () => {
+        if( showPopup === false){
+            setShowpopup(true);
+        }else{
+            setShowpopup(false);
+        }
+    }
+
+
     return (
         <div className="main__container">
             <div className="main__left">
@@ -16,7 +29,19 @@ const Main = () => {
                         <div className="content__under">
                             <div className="under__date"> 2023 / 03 / 29</div>
                             <div className="under__btn">
-                                <button className="show__sendList_popup">+ New Task</button>
+                                <button className="open" onClick={togglePopup}>+ New Task</button>
+                                {showPopup ? (
+                                    <div className="popup">
+                                        <div className="popup_inner">
+                                            <h2>To Do List</h2>
+                                            <div className="popup_inputs">
+                                                <div className="input_date">{parseDate}</div>
+                                                <input typs="text" className="text_list" placeholder='write what will you do'></input>
+                                                <button className="add_list close" onClick={togglePopup}>Add List</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ) : null}
                             </div>
                         </div>
                     </div>
