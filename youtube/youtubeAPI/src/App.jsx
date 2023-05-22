cc1aff4d3e5ea96ca19649478e33fdc10f0a0679
Hyejin Cho <jhj191058@gmail.com>
2023-05-22 19:03:44 +0900

    prepare youtube api

---
+++ youtube/youtubeAPI/src/App.jsx
@@ -0,0 +1,15 @@
+import Main from './Pages/Main';
+import Lists from './Pages/Lists';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <Main/>
+      <Lists/>
+    </>
+  )
+}
+
+export default App
