9e14149de7b2005033dcd72c8e261eec60da8ca6
Emma-Hyejin <cjd191058@naver.com>
2023-06-09 21:35:58 +0900

    DELETE: 파일 삭제 및 재 생성

--- vite-tailwind-project/tailwind.config.js
+++ vite-tailwind-project/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{html, js, tsx, jsx, ts}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
   },
